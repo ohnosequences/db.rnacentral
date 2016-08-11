5c8f911129484ad8f349948a5b728943339e135b
Alexey Alekhin <laughedelic@gmail.com>
2016-08-11 21:24:37 +0200

    Added instance termination on success

--- src/test/scala/generateData.scala
+++ src/test/scala/generateData.scala
@@ -5,11 +5,17 @@ import era7.defaults._
 
 class GenerateDataTests extends org.scalatest.FunSuite {
 
+  val user = awsUsers.aalekhin
 
   test("Run RNAcentral mirroring and wait for the result", ReleaseOnlyTest) {
 
-    assert {
-      rnacentral.launchAndWait(compats.MirrorRNAcentral5, awsUsers.aalekhin)
-    }
+    rnacentral.launchAndMonitor(
+      user,
+      compats.MirrorRNAcentral5,
+      terminateOnSuccess = true
+    ).fold(
+      { msg => fail(msg) },
+      { msg => info(msg); assert(true) }
+    )
   }
 }
--- src/test/scala/runBundles.scala
+++ src/test/scala/runBundles.scala
@@ -11,60 +11,62 @@ case object rnacentral {
   def defaultSpecs[B <: AnyBundle](
     compat: compats.DefaultCompatible[B],
     user: AWSUser
-  ) = //: InstanceSpecs =
-    compat.instanceSpecs(
-      c3.large,
-      user.keypair.name,
-      Some(ec2Roles.projects.name)
-    )
+  ) = compat.instanceSpecs(
+    c3.large,
+    user.keypair.name,
+    Some(ec2Roles.projects.name)
+  )
 
   // use `sbt test:console`:
   // > ohnosequences.db.test.bundles.runBundle(...)
   def runBundle[B <: AnyBundle](
-    compat: compats.DefaultCompatible[B],
-    user: AWSUser
-  ): List[String] =
-    EC2.create(user.profile)
-      .runInstances(1, defaultSpecs(compat, user))
-      .map { _.getInstanceId }
-
-  /* This runs a bundle  */
-  def launchAndWait[B <: AnyBundle](
-    compat: compats.DefaultCompatible[B],
-    user: AWSUser
-  ): Boolean = {
-
+    user: AWSUser,
+    compat: compats.DefaultCompatible[B]
+  ): Option[EC2#Instance] =
     EC2.create(user.profile)
       .runInstances(1, defaultSpecs(compat, user))
       .headOption
-      .map { inst =>
 
-        def checkStatus: String = inst.getTagValue("statika-status").getOrElse("...")
+  /* This runs a bundle  */
+  def launchAndMonitor[B <: AnyBundle](
+    user: AWSUser,
+    compat: compats.DefaultCompatible[B],
+    terminateOnSuccess: Boolean
+  ): Either[String, String] = {
 
-        val id = inst.getInstanceId()
-        def printStatus(st: String) = println(s"${compat.toString} (${id}): ${st}")
+    runBundle(user, compat).map { inst =>
 
-        printStatus("launched")
+      def checkStatus: String = inst.getTagValue("statika-status").getOrElse("...")
 
-        while(checkStatus != "preparing") { Thread sleep 2000 }
-        printStatus("url: "+inst.getPublicDNS().getOrElse("..."))
+      val id = inst.getInstanceId()
+      def printStatus(st: String) = println(s"${compat.toString} (${id}): ${st}")
 
-        @annotation.tailrec
-        def waitForSuccess(previous: String): String = {
-          val current = checkStatus
-          if(current == "failure" || current == "success") {
-            printStatus(current)
-            current
-          } else {
-            if (current != previous) printStatus(current)
-            Thread sleep 3000
-            waitForSuccess(current)
-          }
+      printStatus("launched")
+
+      while(checkStatus != "preparing") { Thread sleep 2000 }
+      printStatus("url: "+inst.getPublicDNS().getOrElse("..."))
+
+      @annotation.tailrec
+      def waitForCompletion(previous: String): String = {
+        val current = checkStatus
+        if(current == "failure" || current == "success") {
+          printStatus(current)
+          current
+        } else {
+          if (current != previous) printStatus(current)
+          Thread sleep 3000
+          waitForCompletion(current)
         }
-
-        waitForSuccess(checkStatus) == "success"
       }
-      .getOrElse(false)
+
+      if (waitForCompletion(checkStatus) != "success") {
+        Left(s"Bundle launch has failed. Instance ${id} is left running for you to check logs.")
+      } else {
+        if (terminateOnSuccess) { inst.terminate() }
+        Right(s"Bundle launch finished successfully.")
+      }
+    }
+    .getOrElse(Left("Couldn't launch an instance. Check your AWS credentials."))
   }
 
 }
