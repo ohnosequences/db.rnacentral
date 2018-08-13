091da342c59271796fd04e22dda1bd4152f38569
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2018-08-13 11:46:19 +0200

    remove all bundle code, make test stub compile

--- src/test/scala/mirrorInS3.scala
+++ src/test/scala/mirrorInS3.scala
@@ -1,43 +1,49 @@
-
 package ohnosequences.db.rnacentral.test
 
-import java.io.file
+import ohnosequences.db.rnacentral._
+import java.io.File
+import ohnosequences.test.ReleaseOnlyTest
+import org.scalatest.FunSuite
 
 class MirrorInS3 extends FunSuite {
 
   test("Check previous releases") {
 
-    Version.all 
-      .map { v => (v, data everything v) } 
-      .foreach { case (v,objs) =>
-        // TODO check files are there
-        ???
+    Version.all
+      .map { v =>
+        (v, data everything v)
+      }
+      .foreach {
+        case (v, objs) =>
+          // TODO check files are there
+          ???
       }
   }
 
-  test("Mirror latest release") {
+  test("Mirror latest release", ReleaseOnlyTest) {
 
     val version = Version.latest
 
-    val tmpFolder = 
+    val tmpFolder =
       new File(s"./data/${version.name}/")
 
     val tmpIdMappingTSVGZFile =
-      new File(tmp, data.input.idMappingTSVGZ)
-    
+      new File(tmpFolder, data.input.idMappingTSVGZ)
+
     val tmpIdMappingTSVFile =
-      new File(tmp, data.input.idMappingTSV)
+      new File(tmpFolder, data.input.idMappingTSV)
 
     val tmpSpeciesSpecificFASTAFile =
-      new File(tmp, data.input.speciesSpecificFASTA)
+      new File(tmpFolder, data.input.speciesSpecificFASTA)
 
-    assert {    
-        OK == failFast(
-        downloadTo(idMappingTSVGZURL, tmpIdMappingTSVGZFile) >=>
-        extractTo(tmpIdMappingTSVGZFile, tmpIdMappingTSVFile) >=>
-        uploadTo(tmpIdMappingTSVFile, data idMappingTSV version)
-      )
-    }
+    // TODO needs code for files and S3 interaction
+    // assert {
+    //   Right(OK) == failFast(
+    //     downloadTo(idMappingTSVGZURL, tmpIdMappingTSVGZFile) >=>
+    //       extractTo(tmpIdMappingTSVGZFile, tmpIdMappingTSVFile) >=>
+    //       uploadTo(tmpIdMappingTSVFile, data idMappingTSV version)
+    //   )
+    // }
 
     // analogously for FASTA
   }
