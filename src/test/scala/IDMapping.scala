1b0c1aa3647c3c3be2ec1cf906d6154f8e050da6
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2018-08-21 14:11:03 +0200

    add a couple of new databases

--- src/test/scala/IDMapping.scala
+++ src/test/scala/IDMapping.scala
@@ -6,19 +6,20 @@ import ohnosequences.db.rnacentral._
 
 class IDMapping extends FunSuite {
 
+  test("parse all EntryAnnotations", ReleaseOnlyTest) {
+
+    def entries =
+      IDMapping entryAnnotations (
+        iterators right (
+          IDMapping rows data.rnacentralData(Version.latest)
+        )
+      )
+
+    assert { allRight(entries) }
+  }
+
   test("well-formed tsv", ReleaseOnlyTest) {
 
     assert { allRight(IDMapping rows data.rnacentralData(Version.latest)) }
-  }
-
-  test("parse all EntryAnnotations", ReleaseOnlyTest) {
-
-    assert {
-      allRight {
-        IDMapping entryAnnotations {
-          iterators right (IDMapping rows data.rnacentralData(Version.latest))
-        }
-      }
-    }
   }
 }
