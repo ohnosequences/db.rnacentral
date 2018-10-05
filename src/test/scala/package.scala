69a81984c9d60c8fb2e217082f73b94b20e0d1ed
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2018-10-05 14:06:04 +0200

    Make auxiliary functions in test private

--- src/test/scala/package.scala
+++ src/test/scala/package.scala
@@ -15,12 +15,21 @@ package object test {
     }
 
   private[test] val s3Client = AmazonS3ClientBuilder.standard().build()
+  private val partSize5MiB   = 5 * 1024 * 1024
 
-  def getCheckedFile(s3Obj: S3ObjectId, file: File) =
+  /**
+    * A partial application of the s3 getCheckedFile function, using the S3
+    * client declared above.
+    */
+  private[test] def getCheckedFile(s3Obj: S3ObjectId, file: File) =
     request.getCheckedFile(s3Client)(s3Obj, file)
 
-  def paranoidPutFile(file: File, s3Obj: S3ObjectId) =
-    request.paranoidPutFile(s3Client)(file, s3Obj, 5 * 1024 * 1024)(
+  /**
+    * A partial application of the s3 paranoidPutFile function, using the S3
+    * client and the part size declared above.
+    */
+  private[test] def paranoidPutFile(file: File, s3Obj: S3ObjectId) =
+    request.paranoidPutFile(s3Client)(file, s3Obj, partSize5MiB)(
       rnacentral.data.hashingFunction
     )
 }
